30d659676d5a1df159f2bec65b99d0c935626295
dandiep <dandiep@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2004-11-06 21:26:57 +0000

    Update the server side handler to something that (may) actually works.
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@59 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

--- xfire-xmlbeans/src/main/org/codehaus/xfire/xmlbeans/XMLBeansHandler.java
+++ xfire-xmlbeans/src/main/org/codehaus/xfire/xmlbeans/XMLBeansHandler.java
@@ -1,18 +1,38 @@
 package org.codehaus.xfire.xmlbeans;
 
+import javax.xml.stream.XMLStreamReader;
+
 import org.apache.xmlbeans.XmlObject;
 import org.codehaus.xfire.MessageContext;
-import org.codehaus.xfire.fault.XFireFault;
+import org.codehaus.xfire.handler.AbstractHandler;
+import org.codehaus.xfire.util.STAXUtils;
 
 /**
  * An XMLBeans handler which can be used in SOAP and REST services.
+ * Override handleObject and getResponseObject to add in your service
+ * logic.
  * 
  * @author <a href="mailto:dan@envoisolutions.com">Dan Diephouse</a>
  * @since Oct 31, 2004
  */
-public interface XMLBeansHandler
+public abstract class XMLBeansHandler
+    extends AbstractHandler
 {
-    void handleObject( XmlObject body, MessageContext context ) throws XFireFault;
+    public abstract void handleObject( XmlObject request, MessageContext context ) 
+        throws Exception;
     
-    XmlObject getResponseObject( MessageContext context );
+    public abstract XmlObject getResponseObject( MessageContext context ) 
+        throws Exception;
+
+    public void invoke(MessageContext context, XMLStreamReader reader) throws Exception
+    {
+        XmlObject request = XmlObject.Factory.parse(reader);
+        handleObject( request, context );
+        
+        XmlObject response = getResponseObject(context);
+        if ( response != null )
+        {
+            STAXUtils.copy(response.newXMLStreamReader(), getXMLStreamWriter(context));
+        }
+    }
 }
