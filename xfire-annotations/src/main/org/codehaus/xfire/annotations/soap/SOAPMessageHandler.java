6b685ccaf5062752abd21f79c74f1bf74ab4502b
poutsma <poutsma@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2005-04-18 21:14:19 +0000

    Removed erroneous copyright notices.
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@331 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

--- xfire-annotations/src/main/org/codehaus/xfire/annotations/soap/SOAPMessageHandler.java
+++ xfire-annotations/src/main/org/codehaus/xfire/annotations/soap/SOAPMessageHandler.java
@@ -1,14 +1,3 @@
-/*
- * Copyright (c) 2005 JTeam B.V.
- * www.jteam.nl
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * JTeam B.V. ("Confidential Information").
- * You shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement you
- * entered into with JTeam.
- */
 package org.codehaus.xfire.annotations.soap;
 
 /**
