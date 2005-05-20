f723623cf43ecfc9e381f319ce1b87efbcf7f918
juze <juze@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2005-05-20 19:18:08 +0000

    Initial XFireHttpServer commit. Its rely on Jetty. New depedency! Eclipse users please run "maven eclipse". I still need to commit tests but Im waiting for dynamic clients in order to not introduce XMLBeans at this project, which is a pain.
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@387 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

---
+++ xfire-core/src/main/org/codehaus/xfire/server/http/XFireHttpServer.java
@@ -0,0 +1,64 @@
+package org.codehaus.xfire.server.http;
+
+import org.codehaus.xfire.server.XFireServer;
+import org.mortbay.http.HttpContext;
+import org.mortbay.http.HttpServer;
+import org.mortbay.jetty.servlet.ServletHandler;
+import org.mortbay.util.InetAddrPort;
+
+/**
+ * HTTP Server for XFire services.
+ * 
+ * @version $Id$
+ */
+public class XFireHttpServer
+    implements XFireServer
+{
+    // components
+    private HttpServer httpServer;
+
+    // properties
+    private int port = 8081;
+
+    public void start()
+        throws Exception
+    {
+        if (isStarted()) {
+            return;
+        }
+        
+        httpServer = new HttpServer();
+        httpServer.addListener(new InetAddrPort(port));
+        HttpContext context = httpServer.getContext("/");
+        ServletHandler handler = new ServletHandler();
+        handler.addServlet("XFireServlet", "/*", "org.codehaus.xfire.transport.http.XFireServlet");
+        context.addHandler(handler);
+        httpServer.start();
+    }
+
+    public void stop()
+        throws Exception
+    {
+        if (isStarted())
+        {
+            httpServer.stop();
+            httpServer = null;
+        }
+    }
+
+    public boolean isStarted()
+    {
+        return (httpServer != null) || httpServer.isStarted();
+    }
+
+    public int getPort()
+    {
+        return port;
+    }
+
+    public void setPort(int value)
+    {
+        port = value;
+    }
+
+}
