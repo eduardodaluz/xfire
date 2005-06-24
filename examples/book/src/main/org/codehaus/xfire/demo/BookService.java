3bab6b4036859b96c32d903cbbec4dd4ec05e617
juze <juze@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2005-06-24 05:13:38 +0000

    Examples now goes at xfire tree. Automated distribution at xfire-all now includes every module jar and confluence pages.
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@520 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

---
+++ examples/book/src/main/org/codehaus/xfire/demo/BookService.java
@@ -0,0 +1,30 @@
+package org.codehaus.xfire.demo;
+
+/**
+ * @author <a href="mailto:dan@envoisolutions.com">Dan Diephouse</a>
+ */
+public class BookService
+{
+    private Book onlyBook;
+    
+    public BookService()
+    {
+        onlyBook = new Book();
+        onlyBook.setAuthor("Dan Diephouse");
+        onlyBook.setTitle("Using XFire");
+        onlyBook.setIsbn("0123456789");
+    }
+
+    public Book[] getBooks()
+    {
+        return new Book[] { onlyBook };
+    }
+    
+    public Book findBook(String isbn)
+    {
+        if (isbn.equals(onlyBook.getIsbn()))
+            return onlyBook;
+        
+        return null;
+    }
+}
