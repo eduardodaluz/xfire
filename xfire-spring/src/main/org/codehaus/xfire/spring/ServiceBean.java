b5193bcbce5ab50085dfed0e638258f3091e4c72
tomeks <tomeks@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2005-11-01 21:18:31 +0000

    xbean config : xfire property changed to protected scope
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@746 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

--- xfire-spring/src/main/org/codehaus/xfire/spring/ServiceBean.java
+++ xfire-spring/src/main/org/codehaus/xfire/spring/ServiceBean.java
@@ -9,15 +9,18 @@ import org.codehaus.xfire.service.Service;
 import org.codehaus.xfire.service.ServiceFactory;
 import org.codehaus.xfire.service.binding.AbstractBinding;
 import org.codehaus.xfire.service.binding.BeanInvoker;
+import org.codehaus.xfire.service.binding.ObjectServiceFactory;
 import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.InitializingBean;
-
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 /**
- * A convenience bean which creates a Service from a ServiceFactory instance. Alternatively, the
- * Jsr181BeanPostProcessor may be used.
- *
+ * A convenience bean which creates a Service from a ServiceFactory instance.
+ * Alternatively, the Jsr181BeanPostProcessor may be used.
+ * 
  * @see org.codehaus.xfire.service.Service
  * @see org.codehaus.xfire.spring.Jsr181BeanPostProcessor
  * @author <a href="mailto:dan@envoisolutions.com">Dan Diephouse </a>
@@ -27,21 +30,31 @@ public class ServiceBean
     implements InitializingBean, BeanNameAware
 {
     private final static Log logger = LogFactory.getLog(ServiceBean.class);
-    
+
     private Service xfireService;
+
     private ServiceFactory serviceFactory;
-    private XFire xFire;
-    private String name;
-    private String namespace;
-    private String beanName;
-    private Class serviceInterface;
-    private Object service;
-    private List inHandlers;
-    private List outHandlers;
-    private List faultHandlers;
     
+    protected XFire xFire;
+    
+    private String name;
+
+    private String namespace;
+
+    private String beanName;
+
+    private Class serviceInterface;
+
+    private Object service;
+
+    private List inHandlers;
+
+    private List outHandlers;
+
+    private List faultHandlers;
+
     public void afterPropertiesSet()
-            throws Exception
+        throws Exception
     {
         // Use specific name if given, else fall back to bean name.
         String theName = (this.name != null ? this.name : this.beanName);
@@ -49,44 +62,42 @@ public class ServiceBean
         {
             theName = theName.substring(1);
         }
-        
+
         Class intf = getServiceInterface();
-        if (intf == null) intf = getService().getClass();
+        if (intf == null)
+            intf = getService().getClass();
+
         
-        xfireService = serviceFactory.create(intf,
-                                         theName,
-                                         namespace,
-                                         null);
-        
+        xfireService = serviceFactory.create(intf, theName, namespace, null);
+
         AbstractBinding binding = (AbstractBinding) xfireService.getBinding();
         if (logger.isInfoEnabled())
         {
-            logger.info("Exposing SOAP v." + xfireService.getSoapVersion().getVersion() + 
-                        " service " + xfireService.getName() + " as " + binding.getStyle() + 
-                        "/" + binding.getUse());
+            logger.info("Exposing SOAP v." + xfireService.getSoapVersion().getVersion()
+                    + " service " + xfireService.getName() + " as " + binding.getStyle() + "/"
+                    + binding.getUse());
         }
 
         xFire.getServiceRegistry().register(xfireService);
-
         if (serviceInterface != null)
             binding.setInvoker(new BeanInvoker(getProxyForService()));
         else
             binding.setInvoker(new BeanInvoker(getService()));
-        
+
         // set up in handlers
-        if (xfireService.getInHandlers() == null) 
+        if (xfireService.getInHandlers() == null)
             xfireService.setInHandlers(getInHandlers());
         else if (getInHandlers() != null)
             xfireService.getInHandlers().addAll(getInHandlers());
 
         // set up out handlers
-        if (xfireService.getOutHandlers() == null) 
+        if (xfireService.getOutHandlers() == null)
             xfireService.setOutHandlers(getOutHandlers());
         else if (getOutHandlers() != null)
             xfireService.getOutHandlers().addAll(getOutHandlers());
 
-        // set up fault handlers
-        if (xfireService.getFaultHandlers() == null) 
+        // set up fault handlers.
+        if (xfireService.getFaultHandlers() == null)
             xfireService.setFaultHandlers(getFaultHandlers());
         else if (getFaultHandlers() != null)
             xfireService.getFaultHandlers().addAll(getFaultHandlers());
@@ -131,16 +142,11 @@ public class ServiceBean
         this.serviceFactory = serviceFactory;
     }
 
-    public void setXfire(XFire xFire)
+    public ServiceFactory getServiceFactory()
     {
-        this.xFire = xFire;
+        return this.serviceFactory;
     }
 
-    public XFire getXfire()
-    {
-        return xFire;
-    }
-    
     /**
      * Sets the service name. Default is the bean name of this exporter.
      */
@@ -150,8 +156,8 @@ public class ServiceBean
     }
 
     /**
-     * Sets the service default namespace. Default is a namespace based on the package of the {@link
-     * #getServiceInterface() service interface}.
+     * Sets the service default namespace. Default is a namespace based on the
+     * package of the {@link #getServiceInterface() service interface}.
      */
     public void setNamespace(String namespace)
     {
@@ -192,4 +198,15 @@ public class ServiceBean
     {
         this.outHandlers = outHandlers;
     }
+
+    public void setXfire(XFire xFire)
+    {
+        this.xFire = xFire;
+    }
+
+    public XFire getXfire()
+    {
+        return xFire;
+    }
+
 }
