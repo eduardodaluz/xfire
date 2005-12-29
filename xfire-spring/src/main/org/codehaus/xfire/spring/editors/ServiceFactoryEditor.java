82670b0db6c9ef9e057582d846d753c575408eb8
dandiep <dandiep@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2005-12-29 03:21:48 +0000

    pass along the transport manager
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@1074 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

--- xfire-spring/src/main/org/codehaus/xfire/spring/editors/ServiceFactoryEditor.java
+++ xfire-spring/src/main/org/codehaus/xfire/spring/editors/ServiceFactoryEditor.java
@@ -4,6 +4,7 @@ import java.beans.PropertyEditorSupport;
 
 import org.codehaus.xfire.service.ServiceFactory;
 import org.codehaus.xfire.spring.config.ServiceFactoryBean;
+import org.codehaus.xfire.transport.TransportManager;
 
 /**
  * @author <a href="mailto:tsztelak@gmail.com">Tomasz Sztelak</a>
@@ -12,11 +13,23 @@ import org.codehaus.xfire.spring.config.ServiceFactoryBean;
 public class ServiceFactoryEditor
     extends PropertyEditorSupport
 {
+    private TransportManager transportManager;
+    
+    public TransportManager getTransportManager()
+    {
+        return transportManager;
+    }
+
+    public void setTransportManager(TransportManager transportManager)
+    {
+        this.transportManager = transportManager;
+    }
 
     public void setAsText(String text)
         throws IllegalArgumentException
     {
         ServiceFactoryBean factoryBean = new ServiceFactoryBean(text);
+        factoryBean.setTransportManager(transportManager);
         ServiceFactory factory;
         try
         {
