0d5e8f1fce86a259d6003fb073049f6382e44908
dandiep <dandiep@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2006-02-05 06:39:39 +0000

    Fix test which didn't compile
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@1158 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

--- xfire-generator/src/test/jsr181/jaxb/globalweather/GlobalWeatherCustomImpl.java
+++ xfire-generator/src/test/jsr181/jaxb/globalweather/GlobalWeatherCustomImpl.java
@@ -7,8 +7,6 @@ import javax.jws.soap.SOAPBinding.ParameterStyle;
 import javax.jws.soap.SOAPBinding.Style;
 import javax.jws.soap.SOAPBinding.Use;
 
-import org.codehaus.xfire.fault.XFireFault;
-
 @WebService(serviceName = "GlobalWeather", targetNamespace = "http://www.webserviceX.NET", endpointInterface = "jsr181.jaxb.globalweather.GlobalWeatherSoap")
 @SOAPBinding(style = Style.DOCUMENT, use = Use.LITERAL, parameterStyle = ParameterStyle.WRAPPED)
 public class GlobalWeatherCustomImpl
@@ -16,13 +14,11 @@ public class GlobalWeatherCustomImpl
 {
 
     public String GetCitiesByCountry(String CountryName)
-        throws XFireFault
     {
         return null;
     }
 
     public String GetWeather(String CityName, String CountryName)
-        throws XFireFault
     {
         return "foo";
     }
