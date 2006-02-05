6a79bf23228ae62668d171bdd576f8939f0e2788
dandiep <dandiep@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2006-02-05 05:50:21 +0000

    Add fault test
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@1155 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

---
+++ xfire-jaxws/src/test/org/codehaus/xfire/jaxws/services/AuthTest.java
@@ -0,0 +1,38 @@
+package org.codehaus.xfire.jaxws.services;
+
+import javax.xml.ws.Endpoint;
+
+import org.codehaus.xfire.jaxws.AbstractJAXWSHttpTest;
+
+import services.auth.AuthServicePortType;
+import services.auth.AuthServiceService;
+import services.auth.AuthenticationFault_Exception;
+
+public class AuthTest
+    extends AbstractJAXWSHttpTest
+{
+    @Override
+    protected void setUp()
+        throws Exception
+    {
+        super.setUp();
+        
+        Endpoint.publish("http://localhost:8191/AuthService", new AuthServiceImpl());
+    }
+
+    public void testService() throws Exception
+    {
+        AuthServiceService service = new AuthServiceService();
+        
+        AuthServicePortType auth = service.getAuthServiceHttpPort();
+
+        try
+        {
+            auth.authenticate("bleh", "bar");
+            fail("Must throw fault.");
+        }
+        catch (AuthenticationFault_Exception f)
+        {
+        }
+    }
+}
