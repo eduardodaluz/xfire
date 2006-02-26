b3ac933924f30491b27b8a7cd66d8e53ea729884
tomeks <tomeks@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2006-02-26 22:26:28 +0000

    +: signature example
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@1270 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

---
+++ xfire-sandbox/xfire-ws-security/src/example/book-ws/src/main/org/codehaus/xfire/client/BookClient.java
@@ -0,0 +1,15 @@
+package org.codehaus.xfire.client;
+
+public class BookClient
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ xfire-sandbox/xfire-ws-security/src/example/book-ws/src/main/org/codehaus/xfire/client/BookClientSign.java
@@ -0,0 +1,40 @@
+package org.codehaus.xfire.client;
+
+import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.codehaus.xfire.security.impl.SecurityProperties;
+
+/**
+ *  THIS SAMPLE DOESN'T WORK. THERE IS ALWAYS "The signature verification failed" returned.
+ *
+ */
+public class BookClientSign
+    extends BookClientUTPP
+{
+
+    protected Map getSecurityProperties()
+    {
+        Map config = new HashMap();
+        config.put(SecurityProperties.PROP_ACTIONS, "signature");
+        config.put(SecurityProperties.PROP_KEYSTORE_PASS, "keystorePass");
+        config.put(SecurityProperties.PROP_KEYSTORE_FILE, "META-INF/xfire/myPrivatestore.jks");
+        config.put(SecurityProperties.PROP_PRIVATE_ALIAS, "alias");
+        config.put(SecurityProperties.PROP_PRIVATE_PASSWORD, "aliaspass");
+        return config;
+    }
+
+    /**
+     * @param args
+     * @throws MalformedURLException
+     */
+    public static void main(String[] args)
+        throws MalformedURLException
+    {
+        BookClientSign client = new BookClientSign();
+        client.executeClient("http://localhost:8080/bookws/services/BookServiceSign");
+
+    }
+
+}
