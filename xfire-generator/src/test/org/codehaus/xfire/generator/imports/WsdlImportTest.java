6231964f30e12d1d87166c41bcab1a187d284381
dandiep <dandiep@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2006-03-01 21:41:30 +0000

    Add import test
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@1285 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

---
+++ xfire-generator/src/test/org/codehaus/xfire/generator/imports/WsdlImportTest.java
@@ -0,0 +1,27 @@
+package org.codehaus.xfire.generator.imports;
+
+import jsr181.GenerationTestSupport;
+
+import org.codehaus.xfire.gen.Wsdl11Generator;
+
+import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.JDefinedClass;
+
+public class WsdlImportTest
+    extends GenerationTestSupport
+{
+    public void testFault() throws Exception
+    {
+        Wsdl11Generator generator = new Wsdl11Generator();
+        generator.setWsdl(getTestFilePath("src/wsdl/import-test/main.wsdl"));
+        generator.setOutputDirectory("target/test-services");
+        generator.setDestinationPackage("org.codehaus.xfire.generator.imports");
+        generator.setBinding("jaxb");
+        generator.setBaseURI(getTestFilePath("src/wsdl/import-test/"));
+        generator.generate();
+        
+        JCodeModel model = generator.getCodeModel();
+        JDefinedClass echo = model._getClass("org.codehaus.xfire.generator.imports.EchoPortType");
+        assertNotNull(echo);
+    }
+}
