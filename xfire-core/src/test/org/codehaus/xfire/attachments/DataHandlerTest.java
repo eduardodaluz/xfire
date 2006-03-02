3eb9e18fc644d58b2e0280e0603357b8edc5ac7d
lacostej <lacostej@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2006-03-02 17:51:59 +0000

    Fix unit test. Little hack to make the test pass on a Linux environment where the mime types configuration is not read by the activation framework by default.
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@1290 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

--- xfire-core/src/test/org/codehaus/xfire/attachments/DataHandlerTest.java
+++ xfire-core/src/test/org/codehaus/xfire/attachments/DataHandlerTest.java
@@ -3,15 +3,56 @@ package org.codehaus.xfire.attachments;
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
 import javax.activation.FileDataSource;
+import javax.activation.FileTypeMap;
+import javax.activation.MimetypesFileTypeMap;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.FileInputStream;
 
 import org.codehaus.xfire.test.AbstractXFireTest;
 
 /**
+ * This test exercises more the Activation framework than XFire itself.
  * @author <a href="mailto:dan@envoisolutiosn.com">Dan Diephouse</a>
  */
 public class DataHandlerTest
     extends AbstractXFireTest
 {
+    // on some Linux systems, file type mapping is stored in a location
+    // not searched by default, thus breaking this test. Let's override it temporarily
+    private static final FileTypeMap DEFAULT_FILE_TYPE_MAP = FileTypeMap.getDefaultFileTypeMap();
+    private static final FileTypeMap OVERRIDING_FILE_TYPE_MAP = createOverridingFileTypeMap();
+
+    // Note: this is not perfect: in a perfect world /etc/mime.types should be overriden by 
+    // the user ~/.mime.types file. Because of what we do here, it won't.
+    private static final FileTypeMap createOverridingFileTypeMap()
+    {
+        File file = new File("/etc/mime.types");
+        FileTypeMap result = DEFAULT_FILE_TYPE_MAP;
+        if (file.exists())
+        {
+            try {
+                InputStream inputStream = new FileInputStream(file);
+                result = new MimetypesFileTypeMap(inputStream);
+                inputStream.close();
+            } catch(Exception ignored) {
+            }
+        }
+        return result;
+    }
+
+    protected void setUp()
+    {
+        FileTypeMap.setDefaultFileTypeMap(OVERRIDING_FILE_TYPE_MAP);
+    }
+
+    protected void tearDown()
+    {
+        FileTypeMap.setDefaultFileTypeMap(DEFAULT_FILE_TYPE_MAP);
+    }
+
+
     public void testText()
         throws Exception
     {
@@ -22,7 +63,8 @@ public class DataHandlerTest
 
         Object content = handler.getContent();
         assertNotNull(content);
-        assertTrue(content instanceof String);
+        assertTrue("The content data type was not correctly detected. Check you computer/activation framework setup. " 
+                   + "Expected String but got: " + content.getClass(), content instanceof String);
         assertEquals("bleh", content);
     }
     
@@ -43,7 +85,7 @@ public class DataHandlerTest
         
         content = factory.createDataContentHandler("image/tiff").getContent(ds);
         assertNotNull(content);
-        // FIXME: For some reason this fails on some machines...
-        // assertTrue(content instanceof java.awt.Image);
+        assertTrue("The content data type was not correctly detected. Check you computer/activation framework setup. " 
+                   + "Expected java.awt.Image but got: " + content.getClass(), content instanceof java.awt.Image);
     }
 }
