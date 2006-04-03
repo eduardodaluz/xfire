039533316f3a1cf48df38f4628d97b2c1977a656
dandiep <dandiep@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2006-04-03 17:19:02 +0000

    XFIRE-326 - create a default package name if none is specified.
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@1408 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

--- xfire-core/src/main/org/codehaus/xfire/util/NamespaceHelper.java
+++ xfire-core/src/main/org/codehaus/xfire/util/NamespaceHelper.java
@@ -1,5 +1,24 @@
+/*
+ * This file contains software licensed under the Aapache License:
+ * 
+ * Copyright 2001-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.codehaus.xfire.util;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -177,6 +196,114 @@ public class NamespaceHelper
         }
 
         return protocol + "://" + sb.toString();
+    }
+   /**
+    * Method makePackageName
+    *
+    * @param namespace
+    * @return
+    */
+   public static String makePackageName(String namespace) {
+
+       String hostname = null;
+       String path = "";
+
+       // get the target namespace of the document
+       try {
+           URL u = new URL(namespace);
+
+           hostname = u.getHost();
+           path = u.getPath();
+       } catch (MalformedURLException e) {
+           if (namespace.indexOf(":") > -1) {
+               hostname = namespace.substring(namespace.indexOf(":") + 1);
+
+               if (hostname.indexOf("/") > -1) {
+                   hostname = hostname.substring(0, hostname.indexOf("/"));
+               }
+           } else {
+               hostname = namespace;
+           }
+       }
+
+       // if we didn't file a hostname, bail
+       if (hostname == null) {
+           return null;
+       }
+
+       // convert illegal java identifier
+       hostname = hostname.replace('-', '_');
+       path = path.replace('-', '_');
+
+       // chomp off last forward slash in path, if necessary
+       if ((path.length() > 0) && (path.charAt(path.length() - 1) == '/')) {
+           path = path.substring(0, path.length() - 1);
+       }
+
+       // tokenize the hostname and reverse it
+       StringTokenizer st = new StringTokenizer(hostname, ".:");
+       String[] words = new String[st.countTokens()];
+
+       for (int i = 0; i < words.length; ++i) {
+           words[i] = st.nextToken();
+       }
+
+       StringBuffer sb = new StringBuffer(namespace.length());
+
+       for (int i = words.length - 1; i >= 0; --i) {
+           addWordToPackageBuffer(sb, words[i], (i == words.length - 1));
+       }
+
+       // tokenize the path
+       StringTokenizer st2 = new StringTokenizer(path, "/");
+
+       while (st2.hasMoreTokens()) {
+           addWordToPackageBuffer(sb, st2.nextToken(), false);
+       }
+
+       return sb.toString();
+   }
+    
+    /**
+     * Massage <tt>word</tt> into a form suitable for use in a Java package name.
+     * Append it to the target string buffer with a <tt>.</tt> delimiter iff
+     * <tt>word</tt> is not the first word in the package name.
+     *
+     * @param sb        the buffer to append to
+     * @param word      the word to append
+     * @param firstWord a flag indicating whether this is the first word
+     */
+    private static void addWordToPackageBuffer(StringBuffer sb, String word,
+                                               boolean firstWord) {
+
+        if (JavaUtils.isJavaKeyword(word)) {
+            word = JavaUtils.makeNonJavaKeyword(word);
+        }
+
+        // separate with dot after the first word
+        if (!firstWord) {
+            sb.append('.');
+        }
+
+        // prefix digits with underscores
+        if (Character.isDigit(word.charAt(0))) {
+            sb.append('_');
+        }
+
+        // replace periods with underscores
+        if (word.indexOf('.') != -1) {
+            char[] buf = word.toCharArray();
+
+            for (int i = 0; i < word.length(); i++) {
+                if (buf[i] == '.') {
+                    buf[i] = '_';
+                }
+            }
+
+            word = new String(buf);
+        }
+
+        sb.append(word);
     }
 
     /**
