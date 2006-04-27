3550f4b6453d4314cbd44e7c433d304f6fb44ff7
dandiep <dandiep@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2006-04-27 00:37:41 +0000

    extend the test case a bit more to try and reproduce errors...
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@1458 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

--- xfire-aegis/src/test/org/codehaus/xfire/client/ConcatClientTest.java
+++ xfire-aegis/src/test/org/codehaus/xfire/client/ConcatClientTest.java
@@ -7,8 +7,6 @@ import org.codehaus.xfire.aegis.AbstractXFireAegisTest;
 import org.codehaus.xfire.service.Service;
 import org.codehaus.xfire.service.invoker.BeanInvoker;
 import org.codehaus.xfire.transport.local.LocalTransport;
-import org.codehaus.xfire.util.LoggingHandler;
-import org.codehaus.xfire.util.dom.DOMOutHandler;
 
 public class ConcatClientTest
     extends AbstractXFireAegisTest
@@ -22,6 +20,12 @@ public class ConcatClientTest
             {
                 return s1 + s2;
             }
+
+            public String concat(String s1, String s2, String s3)
+            {
+                return s1 + s2 + s3;
+            }
+            
         }));
 
         getServiceRegistry().register(s);
@@ -32,17 +36,20 @@ public class ConcatClientTest
         client.setXFire(getXFire());
         client.setUrl("xfire.local://ConcatService");
         client.setTransport(getTransportManager().getTransport(LocalTransport.BINDING_ID));
-        
-        client.addOutHandler(new LoggingHandler());
-        client.addOutHandler(new DOMOutHandler());
-        
+
         Object[] res = client.invoke("concat", new Object[]{"1", "2"});
         
         assertEquals("12", res[0]);
+        
+        res = client.invoke("concat1", new Object[]{"1", "2", "3"});
+        
+        assertEquals("123", res[0]);
     }
 
     public static interface ConcatService
     {
         String concat(String s1, String s2);
+        
+        String concat(String s1, String s2, String s3);
     }
 }
