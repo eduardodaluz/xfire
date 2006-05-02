ad3d91202e52c307897d5155310dc4eb6174fa95
tomeks <tomeks@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2006-05-02 14:58:57 +0000

    + some comments
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@1526 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

--- examples/ws-security/src/main/org/codehaus/xfire/client/BookClientEnc2Way.java
+++ examples/ws-security/src/main/org/codehaus/xfire/client/BookClientEnc2Way.java
@@ -18,6 +18,17 @@ import org.codehaus.xfire.util.dom.DOMOutHandler;
 /**
  * <a href="mailto:tsztelak@gmail.com">Tomasz Sztelak</a> 
  * Encryption Sample : Encryption of whole message body
+ * 
+ * How it works:
+ * 
+ * Client uses server' public key to encrypt message send to this server.
+ * Info about this key is configured at org/codehaus/xfire/client/outsecurity_enc.properties
+ * We are also sending user name (client-344-839) along with message, to allow server to identyfy us.
+ * Server retirve username inside  ValidateUserTokenHandler class and setup this name on the context 
+ * as WSHandlerConstants.ENCRYPTION_USER 
+ * so it can be used to retrieve client public key used to encrypt back message.
+ * Client uses its own  private key to decrypt response ( configureInProperties method )   
+ * 
  */
 public class BookClientEnc2Way
     extends BookClient
@@ -40,36 +51,41 @@ public class BookClientEnc2Way
      */
     protected void configureOutProperties(Properties properties)
     {
+        // We want to encrypt message and pass user token to server
         String action = WSHandlerConstants.ENCRYPT + " " + WSHandlerConstants.USERNAME_TOKEN;
         properties.setProperty(WSHandlerConstants.ACTION, action);
+        // identify ourself ( this will be send as part of user token )
         properties.setProperty(WSHandlerConstants.USER, "client-344-839");
+        // set user used to encrypt message
         properties.setProperty(WSHandlerConstants.ENCRYPTION_USER, "alias");
-        
-       
         
         // Configuration of public key used to encrypt message goes to
         // properties file.
         properties.setProperty(WSHandlerConstants.ENC_PROP_FILE,
                                "org/codehaus/xfire/client/outsecurity_enc.properties");
-        
-        
-
+        // We want password hashed
         properties.setProperty(WSHandlerConstants.PASSWORD_TYPE, WSConstants.PW_DIGEST);
+        // Specyfy callback class to retrive passwords
         properties.setProperty(WSHandlerConstants.PW_CALLBACK_CLASS, PasswordHandler.class
                 .getName());
     }
 
+    /* (non-Javadoc)
+     * @see org.codehaus.xfire.client.BookClient#getName()
+     */
     protected String getName()
     {
 
         return "Encryption ( 2 directions ) Client";
     }
 
+    /* (non-Javadoc)
+     * @see org.codehaus.xfire.client.BookClient#executeClient(java.lang.String)
+     */
     public void executeClient(String serviceName)
         throws MalformedURLException
     {
         System.out.print("Running client : " + getName() + "\n");
-       // DefaultXFire xfire = (DefaultXFire) XFireFactory.newInstance().getXFire();
 
     
         Service serviceModel = new ObjectServiceFactory().create(IBook.class,
@@ -99,23 +115,19 @@ public class BookClientEnc2Way
 
     }
 
+    /**
+     * @param inProperties
+     */
     private void configureInProperties(Properties inProperties)
     {
         
         inProperties.setProperty(WSHandlerConstants.ACTION, WSHandlerConstants.ENCRYPT );
-        //inProperties.setProperty(WSHandlerConstants.USER, "alias");
         inProperties.setProperty(WSHandlerConstants.USER, "client-344-839");
         inProperties.setProperty(WSHandlerConstants.PW_CALLBACK_CLASS, PasswordHandler.class
                                .getName());
         inProperties.setProperty(WSHandlerConstants.ENABLE_SIGNATURE_CONFIRMATION,"false");
         inProperties.setProperty(WSHandlerConstants.DEC_PROP_FILE,
         "org/codehaus/xfire/client/insecurity_enc.properties");
-        //properties.setProperty(WSHandlerConstants.ENCRYPTION_USER, "alias");
-        // Configuration of public key used to encrypt message goes to
-        // properties file.
-        /*inProperties.setProperty(WSHandlerConstants.DEC_PROP_FILE,
-                               "org/codehaus/xfire/client/outsecurity_enc.properties");
-        */
     }
 
 }
