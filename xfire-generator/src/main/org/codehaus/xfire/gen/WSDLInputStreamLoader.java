53ede7e2c8efb33fceb6edff9193343aea2342e3
dandiep <dandiep@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2006-05-08 14:27:01 +0000

    Better error reporting
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@1575 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

--- xfire-generator/src/main/org/codehaus/xfire/gen/WSDLInputStreamLoader.java
+++ xfire-generator/src/main/org/codehaus/xfire/gen/WSDLInputStreamLoader.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
+
+import org.codehaus.xfire.XFireRuntimeException;
 
 /**
  * @author <a href="mailto:tsztelak@gmail.com">Tomasz Sztelak</a>
@@ -15,18 +18,22 @@ public class WSDLInputStreamLoader
     public InputStream getInputStream(String wsdlUrl)
         throws IOException
     {
-        URL url;
-        if(wsdlUrl.toLowerCase().startsWith("file://")){
-            wsdlUrl = wsdlUrl.substring(7);
-        }
         File file = new File(wsdlUrl);
-        if(file.exists()){
+        if(file.exists()) {
             return new FileInputStream(wsdlUrl);
         }
-        url = new URL(wsdlUrl);
-        InputStream inStream = url.openStream();
-        return inStream;
-
+        
+        try
+        {
+            URL url = new URL(wsdlUrl);
+            InputStream inStream = url.openStream();
+            return inStream;
+        }
+        catch (MalformedURLException e)
+        {
+            // it doesn't exist, and its not a good url, so there is probably a typo in the url
+            throw new XFireRuntimeException("Could not find wsdl at location " + wsdlUrl);
+        }
     }
     
 }
