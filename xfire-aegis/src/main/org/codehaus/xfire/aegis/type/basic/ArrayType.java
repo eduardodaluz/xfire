0526950b36bc9f51e73d7ff0017d94c6a05d08b6
dandiep <dandiep@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2006-06-23 18:14:49 +0000

    Commit ArrayType inheritance fix which didn't make it back to prb.
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@1685 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

--- xfire-aegis/src/main/org/codehaus/xfire/aegis/type/basic/ArrayType.java
+++ xfire-aegis/src/main/org/codehaus/xfire/aegis/type/basic/ArrayType.java
@@ -31,26 +31,22 @@ public class ArrayType
     extends Type
 {
     private QName componentName;
-
     private static final Log logger = LogFactory.getLog(ArrayType.class);
-
     private long minOccurs = 0;
-
     private long maxOccurs = Long.MAX_VALUE;
-
     private boolean flat;
-
+    
     public ArrayType()
     {
     }
-
+    
     public Object readObject(MessageReader reader, MessageContext context)
         throws XFireFault
     {
         try
         {
             Collection values = readCollection(reader, context);
-
+            
             return makeArray(getComponentType().getTypeClass(), values);
         }
         catch (IllegalArgumentException e)
@@ -58,45 +54,44 @@ public class ArrayType
             throw new XFireRuntimeException("Illegal argument.", e);
         }
     }
-
+    
     protected Collection createCollection()
     {
         return new ArrayList();
     }
-
+    
     protected Collection readCollection(MessageReader reader, MessageContext context)
         throws XFireFault
     {
         Collection values = createCollection();
-
-        while (reader.hasMoreElementReaders())
+        
+        while ( reader.hasMoreElementReaders() )
         {
             MessageReader creader = reader.getNextElementReader();
-            Type compType = AegisBindingProvider.getReadType(creader.getXMLStreamReader(),
-                                                             context,
-                                                             getComponentType());
+            Type compType = AegisBindingProvider.getReadType(creader.getXMLStreamReader(), context, getComponentType());
+            
             if (creader.isXsiNil())
-            {
+            {    
                 values.add(null);
                 creader.readToEnd();
             }
             else
             {
-                values.add(compType.readObject(creader, context));
+                values.add( compType.readObject(creader, context) );
             }
 
             // check max occurs
             int size = values.size();
             if (size > maxOccurs)
-                throw new XFireFault("The number of elements in " + getSchemaType()
-                        + " exceeds the maximum of " + maxOccurs, XFireFault.SENDER);
-
+                throw new XFireFault("The number of elements in " + getSchemaType() + 
+                                     " exceeds the maximum of " + maxOccurs, XFireFault.SENDER);
+            
         }
 
         // check min occurs
         if (values.size() < minOccurs)
-            throw new XFireFault("The number of elements in " + getSchemaType()
-                    + " does not meet the minimum of " + minOccurs, XFireFault.SENDER);
+            throw new XFireFault("The number of elements in " + getSchemaType() + 
+                                 " does not meet the minimum of " + minOccurs, XFireFault.SENDER);
         return values;
     }
 
@@ -182,8 +177,8 @@ public class ArrayType
             }
             return array;
         }
-        return values.toArray((Object[]) Array.newInstance(getComponentType().getTypeClass(),
-                                                           values.size()));
+        return values.toArray( (Object[]) Array.newInstance( getComponentType().getTypeClass(), 
+                                                             values.size()) );
     }
 
     public void writeObject(Object values, MessageWriter writer, MessageContext context)
@@ -194,17 +189,25 @@ public class ArrayType
 
         Type type = getComponentType();
 
-        if (type == null)
-            throw new XFireRuntimeException("Couldn't find type for " + type.getTypeClass() + ".");
+        String ns = null;
+        if (type.isAbstract())
+            ns = getSchemaType().getNamespaceURI();
+        else
+            ns = type.getSchemaType().getNamespaceURI();
+        
+        String name = type.getSchemaType().getLocalPart();
+
+        if ( type == null )
+            throw new XFireRuntimeException( "Couldn't find type for " + type.getTypeClass() + "." );
 
         Class arrayType = type.getTypeClass();
-
+        
         if (Object.class.isAssignableFrom(arrayType))
         {
             Object[] objects = (Object[]) values;
             for (int i = 0, n = objects.length; i < n; i++)
             {
-                writeValue(objects[i], writer, context, type);
+                writeValue(objects[i], writer, context, type, name, ns);
             }
         }
         else if (Integer.TYPE.equals(arrayType))
@@ -212,7 +215,7 @@ public class ArrayType
             int[] objects = (int[]) values;
             for (int i = 0, n = objects.length; i < n; i++)
             {
-                writeValue(new Integer(objects[i]), writer, context, type);
+                writeValue(new Integer(objects[i]), writer, context, type, name, ns);
             }
         }
         else if (Long.TYPE.equals(arrayType))
@@ -220,7 +223,7 @@ public class ArrayType
             long[] objects = (long[]) values;
             for (int i = 0, n = objects.length; i < n; i++)
             {
-                writeValue(new Long(objects[i]), writer, context, type);
+                writeValue(new Long(objects[i]), writer, context, type, name, ns);
             }
         }
         else if (Short.TYPE.equals(arrayType))
@@ -228,7 +231,7 @@ public class ArrayType
             short[] objects = (short[]) values;
             for (int i = 0, n = objects.length; i < n; i++)
             {
-                writeValue(new Short(objects[i]), writer, context, type);
+                writeValue(new Short(objects[i]), writer, context, type, name, ns);
             }
         }
         else if (Double.TYPE.equals(arrayType))
@@ -236,7 +239,7 @@ public class ArrayType
             double[] objects = (double[]) values;
             for (int i = 0, n = objects.length; i < n; i++)
             {
-                writeValue(new Double(objects[i]), writer, context, type);
+                writeValue(new Double(objects[i]), writer, context, type, name, ns);
             }
         }
         else if (Float.TYPE.equals(arrayType))
@@ -244,7 +247,7 @@ public class ArrayType
             float[] objects = (float[]) values;
             for (int i = 0, n = objects.length; i < n; i++)
             {
-                writeValue(new Float(objects[i]), writer, context, type);
+                writeValue(new Float(objects[i]), writer, context, type, name, ns);
             }
         }
         else if (Byte.TYPE.equals(arrayType))
@@ -252,7 +255,7 @@ public class ArrayType
             byte[] objects = (byte[]) values;
             for (int i = 0, n = objects.length; i < n; i++)
             {
-                writeValue(new Byte(objects[i]), writer, context, type);
+                writeValue(new Byte(objects[i]), writer, context, type, name, ns);
             }
         }
         else if (Boolean.TYPE.equals(arrayType))
@@ -260,7 +263,7 @@ public class ArrayType
             boolean[] objects = (boolean[]) values;
             for (int i = 0, n = objects.length; i < n; i++)
             {
-                writeValue(new Boolean(objects[i]), writer, context, type);
+                writeValue(new Boolean(objects[i]), writer, context, type, name, ns);
             }
         }
         else if (Character.TYPE.equals(arrayType))
@@ -268,82 +271,79 @@ public class ArrayType
             char[] objects = (char[]) values;
             for (int i = 0, n = objects.length; i < n; i++)
             {
-                writeValue(new Character(objects[i]), writer, context, type);
+                writeValue(new Character(objects[i]), writer, context, type, name, ns);
             }
-        }
+        }        
     }
 
-    protected void writeValue(Object value, MessageWriter writer, MessageContext context, Type type)
+    protected void writeValue(Object value, 
+                              MessageWriter writer, 
+                              MessageContext context, 
+                              Type type,
+                              String name,
+                              String ns) 
         throws XFireFault
     {
         type = AegisBindingProvider.getWriteType(context, value, type);
-
-        String ns = null;
-        if (type.isAbstract())
-            ns = getSchemaType().getNamespaceURI();
-        else
-            ns = type.getSchemaType().getNamespaceURI();
-
-        String name = type.getSchemaType().getLocalPart();
-
         MessageWriter cwriter = writer.getElementWriter(name, ns);
-
-        if (value == null && type.isNillable())
+        
+        if (value==null && type.isNillable())
             cwriter.writeXsiNil();
         else
-            type.writeObject(value, cwriter, context);
+            type.writeObject( value, cwriter, context );
 
         cwriter.close();
     }
-
+    
     public void writeSchema(Element root)
     {
         try
         {
-            Element complex = new Element("complexType", SoapConstants.XSD_PREFIX,
-                    SoapConstants.XSD);
+            Element complex = new Element("complexType",
+                                          SoapConstants.XSD_PREFIX,
+                                          SoapConstants.XSD);
             complex.setAttribute(new Attribute("name", getSchemaType().getLocalPart()));
             root.addContent(complex);
 
-            Element seq = new Element("sequence", SoapConstants.XSD_PREFIX, SoapConstants.XSD);
+            Element seq = new Element("sequence",
+                                      SoapConstants.XSD_PREFIX,
+                                      SoapConstants.XSD);
             complex.addContent(seq);
-
-            Element element = new Element("element", SoapConstants.XSD_PREFIX, SoapConstants.XSD);
+            
+            Element element = new Element("element",
+                                          SoapConstants.XSD_PREFIX,
+                                          SoapConstants.XSD);
             seq.addContent(element);
 
             Type componentType = getComponentType();
-            String prefix = NamespaceHelper.getUniquePrefix((Element) root.getParent(),
-                                                            componentType.getSchemaType()
-                                                                    .getNamespaceURI());
+            String prefix = NamespaceHelper.getUniquePrefix((Element) root.getParent(), 
+                                                            componentType.getSchemaType().getNamespaceURI());
 
-            String typeName = prefix + ":" + componentType.getSchemaType().getLocalPart();
+            String typeName = prefix + ":"
+                    + componentType.getSchemaType().getLocalPart();
 
-            element
-                    .setAttribute(new Attribute("name", componentType.getSchemaType()
-                            .getLocalPart()));
+            element.setAttribute(new Attribute("name", componentType.getSchemaType().getLocalPart()));
             element.setAttribute(new Attribute("type", typeName));
-
+            
             if (componentType.isNillable())
             {
                 element.setAttribute(new Attribute("nillable", "true"));
             }
 
             element.setAttribute(new Attribute("minOccurs", new Long(getMinOccurs()).toString()));
-
+            
             if (maxOccurs == Long.MAX_VALUE)
                 element.setAttribute(new Attribute("maxOccurs", "unbounded"));
             else
-                element
-                        .setAttribute(new Attribute("maxOccurs", new Long(getMaxOccurs())
-                                .toString()));
-
+                element.setAttribute(new Attribute("maxOccurs", new Long(getMaxOccurs()).toString()));
+            
         }
         catch (IllegalArgumentException e)
         {
             throw new XFireRuntimeException("Illegal argument.", e);
         }
     }
-
+    
     /**
      * We need to write a complex type schema for Beans, so return true.
      * 
@@ -353,12 +353,12 @@ public class ArrayType
     {
         return true;
     }
-
+    
     public QName getComponentName()
     {
         return componentName;
     }
-
+    
     public void setComponentName(QName componentName)
     {
         this.componentName = componentName;
@@ -370,12 +370,12 @@ public class ArrayType
     public Set getDependencies()
     {
         Set deps = new HashSet();
-
-        deps.add(getComponentType());
-
+        
+        deps.add( getComponentType() );
+        
         return deps;
     }
-
+    
     /**
      * Get the <code>Type</code> of the elements in the array.
      * 
@@ -384,9 +384,9 @@ public class ArrayType
     public Type getComponentType()
     {
         Class compType = getTypeClass().getComponentType();
-
+        
         Type type;
-
+        
         if (componentName == null)
         {
             type = getTypeMapping().getType(compType);
@@ -394,22 +394,21 @@ public class ArrayType
         else
         {
             type = getTypeMapping().getType(componentName);
-
-            // We couldn't find the type the user specified. One is created
-            // below instead.
+            
+            // We couldn't find the type the user specified. One is created below instead.
             if (type == null)
             {
-                logger.debug("Couldn't find array component type " + componentName
-                        + ". Creating one instead.");
+                logger.debug("Couldn't find array component type " 
+                             + componentName + ". Creating one instead.");
             }
         }
-
+        
         if (type == null)
         {
             type = getTypeMapping().getTypeCreator().createType(compType);
             getTypeMapping().register(type);
         }
-
+        
         return type;
     }
 
