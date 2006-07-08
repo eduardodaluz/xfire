936ae9d28075fffc4436bb30a128e09f3a7c9a3f
tomeks <tomeks@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2006-07-08 20:26:24 +0000

    fill SOAPAction
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@1746 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

--- xfire-core/src/main/org/codehaus/xfire/soap/handler/SoapActionOutHandler.java
+++ xfire-core/src/main/org/codehaus/xfire/soap/handler/SoapActionOutHandler.java
@@ -1,11 +1,11 @@
 package org.codehaus.xfire.soap.handler;
 
 import org.codehaus.xfire.MessageContext;
+import org.codehaus.xfire.addressing.AddressingOperationInfo;
 import org.codehaus.xfire.handler.AbstractHandler;
 import org.codehaus.xfire.handler.Phase;
 import org.codehaus.xfire.service.OperationInfo;
 import org.codehaus.xfire.service.Service;
-import org.codehaus.xfire.soap.AbstractSoapBinding;
 import org.codehaus.xfire.soap.SoapConstants;
 
 /**
@@ -29,9 +29,15 @@ public class SoapActionOutHandler
         if (service == null) return;
         
         OperationInfo op = context.getExchange().getOperation();
-        AbstractSoapBinding binding = (AbstractSoapBinding) context.getBinding();
+        AddressingOperationInfo info = (AddressingOperationInfo) op.getProperty(AddressingOperationInfo.ADDRESSING_OPERATION_KEY);
+        String action = null;
+        if( info != null ){
+            action = info.getOutAction();    
+        }
         
-        String action = binding.getSoapAction(op);
+        /*AbstractSoapBinding binding = (AbstractSoapBinding) context.getBinding();
+        
+        String action = binding.getSoapAction(op);*/
         if (action != null)
             context.getOutMessage().setProperty(SoapConstants.SOAP_ACTION, action);
     }
