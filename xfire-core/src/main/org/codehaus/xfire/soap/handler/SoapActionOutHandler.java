bcadbf3eee252a32abddabb72dec5cb09a2c9f40
dandiep <dandiep@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2006-07-19 19:15:06 +0000

    Revert change. The soap action comes from the SOAP binding, not the ws-addressing information.
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@1763 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

--- xfire-core/src/main/org/codehaus/xfire/soap/handler/SoapActionOutHandler.java
+++ xfire-core/src/main/org/codehaus/xfire/soap/handler/SoapActionOutHandler.java
@@ -1,11 +1,11 @@
 package org.codehaus.xfire.soap.handler;
 
 import org.codehaus.xfire.MessageContext;
-import org.codehaus.xfire.addressing.AddressingOperationInfo;
 import org.codehaus.xfire.handler.AbstractHandler;
 import org.codehaus.xfire.handler.Phase;
 import org.codehaus.xfire.service.OperationInfo;
 import org.codehaus.xfire.service.Service;
+import org.codehaus.xfire.soap.AbstractSoapBinding;
 import org.codehaus.xfire.soap.SoapConstants;
 
 /**
@@ -29,15 +29,9 @@ public class SoapActionOutHandler
         if (service == null) return;
         
         OperationInfo op = context.getExchange().getOperation();
-        AddressingOperationInfo info = (AddressingOperationInfo) op.getProperty(AddressingOperationInfo.ADDRESSING_OPERATION_KEY);
-        String action = null;
-        if( info != null ){
-            action = info.getOutAction();    
-        }
+        AbstractSoapBinding binding = (AbstractSoapBinding) context.getBinding();
         
-        /*AbstractSoapBinding binding = (AbstractSoapBinding) context.getBinding();
-        
-        String action = binding.getSoapAction(op);*/
+        String action = binding.getSoapAction(op);
         if (action != null)
             context.getOutMessage().setProperty(SoapConstants.SOAP_ACTION, action);
     }
