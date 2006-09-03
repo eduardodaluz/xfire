3662dfd8389a103fab7b889d2a1d451c6caca9aa
dandiep <dandiep@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2006-09-03 22:34:28 +0000

    Setting of the correct targetNamespace is now done in ServiceInterfaceGenerator.
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@1896 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

--- xfire-jaxws/src/main/org/codehaus/xfire/jaxws/gen/InterfaceGenerator.java
+++ xfire-jaxws/src/main/org/codehaus/xfire/jaxws/gen/InterfaceGenerator.java
@@ -1,10 +1,5 @@
 package org.codehaus.xfire.jaxws.gen;
 
-import javax.jws.WebService;
-import javax.jws.soap.SOAPBinding;
-import javax.jws.soap.SOAPBinding.ParameterStyle;
-import javax.jws.soap.SOAPBinding.Style;
-import javax.jws.soap.SOAPBinding.Use;
 import javax.xml.namespace.QName;
 import javax.xml.ws.RequestWrapper;
 import javax.xml.ws.ResponseWrapper;
@@ -13,43 +8,15 @@ import org.codehaus.xfire.XFireRuntimeException;
 import org.codehaus.xfire.gen.GenerationContext;
 import org.codehaus.xfire.gen.GenerationException;
 import org.codehaus.xfire.gen.jsr181.ServiceInterfaceGenerator;
-import org.codehaus.xfire.service.Service;
 import org.codehaus.xfire.service.OperationInfo;
 
 import com.sun.codemodel.JAnnotationUse;
-import com.sun.codemodel.JDefinedClass;
 import com.sun.codemodel.JMethod;
 import com.sun.codemodel.JType;
 
 public class InterfaceGenerator
     extends ServiceInterfaceGenerator
 {
-
-    @Override
-    protected void annotate(GenerationContext context, Service service, JDefinedClass jc)
-    {
-        JAnnotationUse ann = jc.annotate(WebService.class);
-        
-        ann.param("name", service.getServiceInfo().getPortType().getLocalPart());
-        // The service interface should have the namespace of the port type
-        ann.param("targetNamespace", service.getServiceInfo().getPortType().getNamespaceURI());
-        
-        service.setProperty(SERVICE_INTERFACE, jc);
-        
-        ann = jc.annotate(SOAPBinding.class);
-        ann.param("style", Style.DOCUMENT);
-        ann.param("use", Use.LITERAL);
-        
-        if (service.getServiceInfo().isWrapped())
-        {
-            ann.param("parameterStyle", ParameterStyle.WRAPPED);
-        }
-        else
-        {
-           ann.param("parameterStyle", ParameterStyle.BARE);
-        }
-    }
-
     @Override
     protected void annotate(GenerationContext context, OperationInfo op, JMethod method) 
     {
