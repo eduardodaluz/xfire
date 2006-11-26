73c6e558a7bbb345126d375cbf5da7dde5d6b3f3
dandiep <dandiep@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2006-11-26 21:01:38 +0000

    Improve attachment file handling - delete the file by default, but allow users to override by doing:
    DeleteOnCloseFileInputStream is = (DeleteOnCloseFileInputStream) source.getInputStream();
    is.setDelete(false);
    
    
    
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@2029 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

--- xfire-core/src/main/org/codehaus/xfire/util/CachedOutputStream.java
+++ xfire-core/src/main/org/codehaus/xfire/util/CachedOutputStream.java
@@ -4,7 +4,6 @@ import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -130,7 +129,7 @@ public class CachedOutputStream extends OutputStream
         {
             try
             {
-                return new FileInputStream(tempFile);
+                return new DeleteOnCloseFileInputStream(tempFile);
             }
             catch (FileNotFoundException e)
             {
