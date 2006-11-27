3e166d0153f74d87a16131a37fa79281bcbc053c
tomeks <tomeks@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2006-11-27 21:32:41 +0000

    regenerate certs and fixed cert build script
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@2040 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

--- xfire-ws-security/src/test/META-INF/xfire/generateKeyPair.bat
+++ xfire-ws-security/src/test/META-INF/xfire/generateKeyPair.bat
@@ -1,11 +1,11 @@
-rem @echo off
-echo alias %1
-echo keypass %2
-echo KeyStorePass %3
+#rem @echo off
+#echo alias %1
+#echo keypass %2
+#echo KeyStorePass %3
 
 
-keytool -genkey -alias %1 -keypass %2 -keystore privatestore.jks -storepass %3  -dname "cn=%1" -keyalg RSA
-keytool -selfcert -alias %1 -keystore privatestore.jks -storepass %3 -keypass %2
-keytool -export -alias %1 -file key.rsa -keystore privatestore.jks -storepass %3
-keytool -import -alias %1 -file key.rsa -keystore publicstore.jks -storepass %3
+keytool -genkey -alias myAlias -keypass myAliasPassword -keystore privatestore.jks -storepass keyStorePassword  -dname "cn=myAlias" -keyalg RSA -validity 356
+keytool -selfcert -alias myAlias -keystore privatestore.jks -storepass keyStorePassword -keypass myAliasPassword
+keytool -export -alias myAlias -file key.rsa -keystore privatestore.jks -storepass keyStorePassword
+keytool -import -alias myAlias -file key.rsa -keystore publicstore.jks -storepass keyStorePassword
 
