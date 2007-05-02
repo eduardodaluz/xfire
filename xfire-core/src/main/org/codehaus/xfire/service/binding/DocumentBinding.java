426522c7726d63448daaedf6b131317d74739b73
tomeks <tomeks@c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba>
2007-05-02 12:21:49 +0000

    namespace duplication fix
    git-svn-id: http://svn.codehaus.org/xfire/trunk/xfire@2161 c7d0bf07-ec0d-0410-b2cc-d48fa9be22ba

--- xfire-core/src/main/org/codehaus/xfire/service/binding/DocumentBinding.java
+++ xfire-core/src/main/org/codehaus/xfire/service/binding/DocumentBinding.java
@@ -1,7 +1,9 @@
 package org.codehaus.xfire.service.binding;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
@@ -14,6 +16,7 @@ import org.codehaus.xfire.service.MessageInfo;
 import org.codehaus.xfire.service.MessagePartInfo;
 import org.codehaus.xfire.service.OperationInfo;
 import org.codehaus.xfire.service.Service;
+import org.codehaus.xfire.util.NamespaceHelper;
 
 
 public class DocumentBinding
@@ -49,6 +52,22 @@ public class DocumentBinding
         {
             msgInfo = op.getOutputMessage();
         }
+        Set namespaces = new HashSet();
+        for(Iterator itr = msgInfo.getMessageParts().iterator(); itr.hasNext();){
+        	MessagePartInfo outParam = (MessagePartInfo) itr.next();
+        	String ns = getBoundNamespace(context, outParam);
+        	namespaces.add(ns);
+        }
+        
+        for(Iterator iter = namespaces.iterator();iter.hasNext();){
+        	String ns = (String) iter.next();
+        	try {
+				NamespaceHelper.getUniquePrefix(writer,ns, true);
+			} catch (XMLStreamException e) {
+				 throw new XFireFault("Could not write to outgoing stream.", e, XFireFault.RECEIVER);
+			}
+        }
+        	
         
         for(Iterator itr = msgInfo.getMessageParts().iterator(); itr.hasNext();)
         {
